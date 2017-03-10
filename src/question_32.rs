bbe2de0594c6914be27ec31cec7f125a88d1f4ec
srf94 <samfawcett@googlemail.com>
2017-03-10 22:43:48 +0000

    question 32 final

--- src/question_32.rs
+++ src/question_32.rs
@@ -1,54 +1,44 @@
-// 100 * 100 = 10000
-// 1000 * 10 = 10000
-// 99 * 99 < 10000 < 100000
-// Must be 5 / 4 split, eg ab * cde = wxyz
+fn pandigital(a: &str, b: &str, c: &str) -> bool {
+	if a.len() + b.len() + c.len() != 9 { return false; }
 
-use std::collections::HashSet;
-
-
-fn get_all_perms (digits: Vec<usize>) -> Vec<usize> {
-	let mut perms = Vec::new();
-	for digit in digits { 
-		let mut digits_copy = digits;
-		digits_copy.remove(digit);
-		
-		let mut sub_perms = get_all_perms(digits_copy);
-		for sub_perm in sub_perms {
-			
+	for digit in "123456789".chars() {
+		if !a.contains(digit) && !b.contains(digit) && !c.contains(digit) {
+		    return false;
 		}
 	}
-	
-	rh_digits
-}
 
-
-fn get_rh_digits (lh_digits: &Vec<usize>) -> Vec<usize> {
-	let mut rh_digits = Vec::new();
-	for i in 1..10 { 
-		if !lh_digits.contains(&i) { rh_digits.push(i); }
-	}
-	rh_digits
-}
-
-
-fn one_four_case (lh_digits: Vec<usize>) -> usize {
-	let rh_digits = get_rh_digits(&lh_digits);
-	
-	for (i, digit) in lh_digits.iter().enumerate() {
-		
-		
-    	println!("{}, {}", i, digit);
-	}
-	
-	0
+    true
 }
 
 
 fn main() {
-	// let mut allowed_products = HashSet::new();
-	let var = vec![1,2,3,4,5];
-	
-    println!("{}", one_four_case(var));
+	let mut products: Vec<usize> = Vec::new();
 
+	for a in 1..10 {
+	for b in 1111..10000 {
+		let c = a * b;
+		if pandigital(&a.to_string()[..], 
+					  &b.to_string()[..],
+					  &c.to_string()[..]) {
+			if !products.contains(&c) {
+				products.push(c);
+			}
+		}		
+	}}
+
+	for a in 1..100 {
+	for b in 111..1000 {
+		let c = a * b;
+		if pandigital(&a.to_string()[..], 
+					  &b.to_string()[..],
+					  &c.to_string()[..]) {
+			if !products.contains(&c) {
+				products.push(c);
+			}		
+		}		
+	}}
+	
+	let product_sum = products.iter().fold(0, |mut sum, &x| {sum += x; sum});
+	println!("Sum of products: {:?}", product_sum);
 }
 
