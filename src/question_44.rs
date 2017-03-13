453cd2a28e6a9ebdea398f2720669d1f084cd4d4
srf94 <samfawcett@googlemail.com>
2017-03-13 21:49:44 +0000

    question 44

--- src/question_44.rs
+++ src/question_44.rs
@@ -1,20 +1,21 @@
 fn main() {
-
-	let max_size = 1000;
+	let max_size = 10000;
 	let mut pents = Vec::new();
 	for n in 1..max_size {
-		pents.push( n*(3*n - 1) / 2);
+		pents.push(n * (3*n - 1) / 2);
 	}
 
-	for i in 0..max_size-1 {
+	for i in 2100..max_size-1 {
+		let pent_i = pents[i];
 		for j in 0..i {
-			if !pents.contains(&(pents[i] - pents[j])) { continue; }
-			if !pents.contains(&(pents[i] + pents[j])) { continue; }
-    		println!("{}, {}", pents[i], pents[j]);
+			let pent_j = pents[j];
+
+			if !pents.contains(&(pent_i - pent_j)) { continue; }
+			if !pents.contains(&(pent_i + pent_j)) { continue; }
+    		println!("Difference: {}", pent_i - pent_j);
 		}
 		if i % 100 == 0 {
     		println!("Checkin: {}", i);
 		}
 	}
-
 }
