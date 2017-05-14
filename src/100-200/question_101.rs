5e09fb02f9d2b7edb71520c0ffe27de1c3ef535f
srf94 <samfawcett@googlemail.com>
2017-05-14 12:19:02 +0100

    question 101 finished

--- src/100-200/question_101.rs
+++ src/100-200/question_101.rs
@@ -1,4 +1,6 @@
-fn calc_fn(n: f64, coefs: &Vec<i64>) -> f64 {
+fn calc_fn(n: f64, coefs: &Vec<f64>) -> f64 {
+	// Given a vector representation of f(n), calculate f(n)
+
 	let mut tot = 0.0 as f64;
 	for (power, coef) in coefs.iter().enumerate() {
 		tot += (*coef as f64) * n.powi(power as i32);
@@ -9,6 +11,7 @@ fn calc_fn(n: f64, coefs: &Vec<i64>) -> f64 {
 
 fn mult_matrix(M: &Vec<Vec<f64>>, N: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
 	// Multiply two matricies with dimensions axb and bxc
+
 	let a = M.len();
 	let b = M[0].len();
 	let b_ = N.len();
@@ -36,6 +39,8 @@ fn mult_matrix(M: &Vec<Vec<f64>>, N: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
 
 
 fn pivot_matrix(M: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+	// Create a pivot matrix as used in the Doolittle LU method
+
     let m = M[0].len();
 
     let mut id_mat = vec![vec![0.0 as f64; m]; m];
@@ -64,6 +69,9 @@ fn pivot_matrix(M: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
 
 
 fn LU_decompose(A: Vec<Vec<f64>>) -> (Vec<Vec<f64>>, Vec<Vec<f64>>, Vec<Vec<f64>>) {
+	// Calculate the LU decomposition of A (using the pivot method)
+	// Used to transform Ax = b into LUx = Pb
+
 	let n = A[0].len();
 	let mut L = vec![vec![0.0 as f64; n]; n];
 	let mut U = vec![vec![0.0 as f64; n]; n];
@@ -96,50 +104,98 @@ fn LU_decompose(A: Vec<Vec<f64>>) -> (Vec<Vec<f64>>, Vec<Vec<f64>>, Vec<Vec<f64>
 }
 
 
+fn solve_LU_system(L: Vec<Vec<f64>>, U: Vec<Vec<f64>>, Pb: Vec<Vec<f64>>) -> Vec<f64>{
+	// Solve equations of the form LUx = Pb by substitution
+
+	let n = Pb.len();
+
+	// First solve Ly = Pb for y
+	let mut y = vec![0.0 as f64; n];
+	for i in 0..n {
+		// Need to subtract already known values of L * y as they become known
+		let mut minus_sum = 0.0 as f64;
+		for j in 0..i {
+			minus_sum += L[i][j] * y[j];
+		}
+
+		y[i] = (Pb[i][0] - minus_sum) / L[i][i];
+	}
+
+	// Now solve Ux = y for x
+	let mut x = vec![0.0 as f64; n];
+	for i in (0..n).rev() {
+		// Need to subtract already known values of U * x as they become known
+		let mut minus_sum = 0.0 as f64;
+		for j in i..n {
+			minus_sum += U[i][j] * x[j];
+		}
+
+		x[i] = (y[i] - minus_sum) / U[i][i];
+	}
+
+	x
+}
+
+
+fn create_Ax_b(n: u32, target_fn: &Vec<f64>) -> (Vec<Vec<f64>>, Vec<Vec<f64>>) {
+	// Create a matrix A and vector b that represents the problem in Ax=b format
+
+	// EXAMPLE
+	// n = 4
+	// a_0 + n*a_1 + n^2*a_2 + n^3*a_3 = f(n) for n = 1, 2, 3, 4
+	// (1  1  1  1)   (a_0) = (f(1))
+	// (1  2  4  16)  (a_1)   (f(2))
+	// (1  3  9  27)  (a_2)   (f(3))
+	// (1  4  16 64)  (a_3)   (f(4))
+
+	let mut A = vec![];
+	for i in 1..n+1 {
+		let mut inner_v = vec![];
+		for j in 0..n {
+			inner_v.push((i as i64).pow(j) as f64);
+		}
+		A.push(inner_v);
+	}
+
+	let mut b = vec![];
+	for i in 1..n+1 {
+		let val = calc_fn(i as f64, target_fn);
+		b.push(vec![val]);
+	}
+
+	(A, b)
+}
+
+
 fn main() {
-	let target_fn = vec![1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1];
-	println!("{:?}", calc_fn(1.0 as f64, &target_fn));
-	println!("{:?}", calc_fn(2.0 as f64, &target_fn));
-	println!("{:?}", calc_fn(3.0 as f64, &target_fn));
-	println!("{:?}", calc_fn(4.0 as f64, &target_fn));
+	// Represent polynomials as a vector of coefficents: f(n) = a_0 + a_1*n + ...
+	let target_fn = vec![1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0];
 
-	// OP(1, n)
-	// a_0 = f(n) for n = 1
-	let A = vec![vec![1]];
+	let mut FIT_sum = 0.0;
+	for n in 1..11 {
+		let tup = create_Ax_b(n, &target_fn);
+		let A = tup.0;
+		let b = tup.1;
 
-	// OP(2, n)
-	// a_0 + n*a_1 = f(n) for n = 1, 2
-	// (1  1) (a_0) = (f(1))
-	// (1  2) (a_1)   (f(2))
-	let A = vec![vec![1.0 as f64, 1.0 as f64], vec![1.0 as f64, 2.0 as f64]];
-	// let b = vec![calc_fn(1, &target_fn), calc_fn(2, &target_fn)];
-	let b = vec![vec![calc_fn(1.0 as f64, &target_fn)], vec![calc_fn(2.0 as f64, &target_fn)]];
+		let tup = LU_decompose(A);
+		let P = tup.0;
+		let L = tup.1;
+		let U = tup.2;
 
-	let tup = LU_decompose(A);
-	let P = tup.0;
-	let L = tup.1;
-	let U = tup.2;
+		let Pb = mult_matrix(&P, &b);
+		let solved = solve_LU_system(L, U, Pb);
 
-	// Ly = Pb
-	let Pb = mult_matrix(&P, &b);
+		// It is possible that the n+1'th term still fit with the target polynomial
+		// To generalise, this should be checked here
+		// However as we have a n^k term for k=1, 10 inclusive we know this cannot happen here
+		FIT_sum += calc_fn((n+1) as f64, &solved);
 
-	let mut y = vec![0.0 as f64; b.len()];
-	y[0] = Pb[0][0] / L[0][0];
-	y[1] = (Pb[1][0] - L[1][0] * y[0]) / L[1][1];
+		println!("n: {:?}", n);
+		println!("Actual value for n+1: {:?}", calc_fn((n+1) as f64, &target_fn));
+		println!("Calculated value for n+1: {:?}", calc_fn((n+1) as f64, &solved));
+		println!("");
+	}
 
-	// Ux = y
-	let mut x = vec![0.0 as f64; b.len()];
-	x[1] = y[1] / U[1][1];
-	x[0] = (y[0] - U[0][1] * y[1])/ U[0][0];
-
-	println!("{:?}", L);
-	println!("{:?}", Pb);
-	println!("{:?}", y);
-
-	println!("{:?}", U);
-	println!("{:?}", x);
-
-	// x = [-681, 682] which is what we expect! This means f(1)=1, f(2)=638 (both correct)
-	// Then f(3) = 1365 - our FIT.
-
+	// I am happy rounding to i64 as we know the final answer must be in interger in this example
+	println!("Sum of FIT: {:?}", FIT_sum as i64);
 }
